1b0d99ad448c6fc6ae3da5e8b4e368d481e296f9
Petr Baiborin <razor267@mail.ru>
2022-01-20 20:00:11 +0300

    class component react, props. LES 92

--- src/components/post-status-filter/post-status-filter.js
+++ src/components/post-status-filter/post-status-filter.js
@@ -3,10 +3,10 @@ import './post-status-filter.css';
 
 const PostStatusFilter = () => {
     return (
-      <div className="btn-group">
-          <button/>
-          <button/>
-      </div>
+        <div className="btn-group">
+            <button type="button" className="btn-info">Все</button>
+            <button type="button" className="btn btn-outline-secondary">Понравилось</button>
+        </div>
     )
 }
 
